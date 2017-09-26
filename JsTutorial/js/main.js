6714b672acd0490c5bbbb03160a872f2b32006af
markwh1te <iamwh1temark@outlook.com>
2017-09-26 16:00:42 +0800

    update

--- JsTutorial/js/main.js
+++ JsTutorial/js/main.js
@@ -1,5 +1,40 @@
 function greetMe(yourName) {
     alert('Hello ' + yourName);
-  }
-  
- greetMe('World');
+}
+greetMe('World');
+
+var fn = x => x * x;
+var fn_object = (x, y) => ({
+    'test': x + y
+})
+var age = 20;
+if (age >= 18) { // 如果age >= 18为true，则执行if语句块
+    dd = fn(age)
+    test12 = fn_object(1, 2)
+    alert(test12['test']);
+} else { // 否则执行else语句块
+    alert('teenager');
+}
+document.title = 'Vivi学习JavaScript!';
+var i, s, menu, drinks;
+
+menu = document.getElementById('drink-menu');
+menu.tagName; // 'DL'
+
+drinks = document.getElementsByTagName('dt');
+var i, s, menu, drinks;
+
+menu = document.getElementById('drink-menu');
+alert(menu.tagName); // 'DL'
+
+drinks = document.getElementsByTagName('dt');
+s = '提供的饮料有:';
+for (i = 0; i < drinks.length; i++) {
+    s = s + drinks[i].innerHTML + ',';
+}
+alert(s);
+s = '提供的饮料有:';
+for (i = 0; i < drinks.length; i++) {
+    s = s + drinks[i].innerHTML + ',';
+}
+alert(s);
