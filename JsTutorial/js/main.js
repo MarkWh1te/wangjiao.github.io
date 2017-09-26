875975d56ce1c49c75bb42bca57e8ef072aee0b9
markwh1te <iamwh1temark@outlook.com>
2017-09-26 18:06:52 +0800

    finished getting data from douban

--- JsTutorial/js/main.js
+++ JsTutorial/js/main.js
@@ -1,5 +1,4 @@
-function greetMe(yourName) {
-    alert('Hello ' + yourName);
+function greetMe(yourName) { alert('Hello ' + yourName);
 }
 greetMe('World');
 
@@ -38,3 +37,54 @@ for (i = 0; i < drinks.length; i++) {
     s = s + drinks[i].innerHTML + ',';
 }
 alert(s);
+
+var p = document.getElementById('drink-menu');
+// p.innerHTML = '<h1>test111</h1>';
+// p.innerText='test';
+// p.style.color = '#ff0000';
+// p.style.fontSize = '20px';
+// p.style.paddingTop = '2em'
+
+function success(text) {
+    var textarea = document.getElementById('drink-menu');
+    textarea.innerHTML = '<img src='+text['image']+'>'+'<br>'+'<h1>'+text['author']+'</h1>';
+    // textarea.innerText = "sssss";
+}
+
+function fail(code) {
+    var textarea = document.getElementById('drink-menu');
+    textarea.innerText = 'Error code: ' + code;
+}
+
+var douban_apiurl = 'https://api.douban.com/v2/book/1220562'
+// var douban_apiurl = 'http://stock.markwh1te.org/'
+
+var responseHandler; // 定义一个全局作用域的函数
+
+function getJSONP(url, cb) {
+if (url.indexOf('?') === -1) {
+    url += '?callback=responseHandler';
+} else {
+    url += '&callback=responseHandler';
+}
+
+// 创建script 标签
+var script = document.createElement('script');
+
+
+// 在函数内部实现包裹函数，因为要用到cb
+responseHandler = function(json) {
+    try {
+    cb(json)
+    } finally {
+    // 函数调用之后不管发生什么都要移除对应的标签，留着也没用
+    script.parentNode.removeChild(script);
+    }
+}
+
+script.setAttribute('src', url)
+document.body.appendChild(script);
+}
+
+
+getJSONP("https://api.douban.com/v2/book/1220562",(e)=>{success(e)})
