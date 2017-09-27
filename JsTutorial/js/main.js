2ce828cd72dafe9770ab336f9e19267d9e735139
markwh1te <iamwh1temark@outlook.com>
2017-09-27 14:59:10 +0800

    format file

--- JsTutorial/js/main.js
+++ JsTutorial/js/main.js
@@ -1,4 +1,5 @@
-function greetMe(yourName) { alert('Hello ' + yourName);
+function greetMe(yourName) {
+    alert('Hello ' + yourName);
 }
 greetMe('World');
 
@@ -47,7 +48,7 @@ var p = document.getElementById('drink-menu');
 
 function success(text) {
     var textarea = document.getElementById('drink-menu');
-    textarea.innerHTML = '<img src='+text['image']+'>'+'<br>'+'<h1>'+text['auther']+'</h1>';
+    textarea.innerHTML = '<img src=' + text['image'] + '>' + '<br>' + '<h1>' + text['auther'] + '</h1>';
     // textarea.innerText = "sssss";
 }
 
@@ -57,34 +58,32 @@ function fail(code) {
 }
 
 var douban_apiurl = 'https://api.douban.com/v2/book/1220562'
-// var douban_apiurl = 'http://stock.markwh1te.org/'
+    // var douban_apiurl = 'http://stock.markwh1te.org/'
 
 var responseHandler; // 定义一个全局作用域的函数
 
 function getJSONP(url, cb) {
-if (url.indexOf('?') === -1) {
-    url += '?callback=responseHandler';
-} else {
-    url += '&callback=responseHandler';
-}
-
-// 创建script 标签
-var script = document.createElement('script');
-
-
-// 在函数内部实现包裹函数，因为要用到cb
-responseHandler = function(json) {
-    try {
-    cb(json)
-    } finally {
-    // 函数调用之后不管发生什么都要移除对应的标签，留着也没用
-    script.parentNode.removeChild(script);
+    if (url.indexOf('?') === -1) {
+        url += '?callback=responseHandler';
+    } else {
+        url += '&callback=responseHandler';
     }
+
+    // 创建script 标签
+    var script = document.createElement('script');
+
+
+    // 在函数内部实现包裹函数，因为要用到cb
+    responseHandler = function(json) {
+        try {
+            cb(json)
+        } finally {
+            // 函数调用之后不管发生什么都要移除对应的标签，留着也没用
+            script.parentNode.removeChild(script);
+        }
+    }
+
+    script.setAttribute('src', url)
+    document.body.appendChild(script);
 }
-
-script.setAttribute('src', url)
-document.body.appendChild(script);
-}
-
-
-getJSONP("https://api.douban.com/v2/book/1220562",(e)=>{success(e)})
+getJSONP("https://api.douban.com/v2/book/1220562", (e) => { success(e) })
